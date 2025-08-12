0abbc68fbda8485385e05212bc37740c64027cae
Taehyeon Kim <Taehyeon@pulsenics.com>
2025-08-12 12:36:34 -0400

    improve responsiveness of bode plot

--- client/src/renderer/src/components/charts/BodePlot.jsx
+++ client/src/renderer/src/components/charts/BodePlot.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, memo, useState, useContext } from 'react';
+import React, { useEffect, useRef, memo, useState, useContext } from 'react'
 import Plot from 'react-plotly.js'
 import { DarkModeContext } from '../../contexts/DarkModeContext'
 import { geekblue, gray } from '@ant-design/colors'
@@ -13,54 +13,78 @@ import { geekblue, gray } from '@ant-design/colors'
  * - yAxisLabel: string              // label for the y‐axis
  */
 const BodePlot = ({ frequencies, measurements, chartId, chartTitle, yAxisLabel }) => {
-  const chartRef = useRef(null);
-  const { darkMode } = useContext(DarkModeContext);
-  const [layout, setLayout] = useState({});
+  const chartRef = useRef(null)
+  const { darkMode } = useContext(DarkModeContext)
+  const [layout, setLayout] = useState({})
+
+  const updateLayoutDimensions = () => {
+    const container = chartRef.current
+    if (container) {
+      const { width: containerWidth, height: containerHeight } = container.getBoundingClientRect()
+      setLayout((prevLayout) => ({
+        ...prevLayout,
+        width: containerWidth,
+        height: containerHeight
+      }))
+    }
+  }
 
   useEffect(() => {
-    // dimensions
-    const container = chartRef.current;
-    if (container) {
-      const { width: containerWidth, height: containerHeight } = container.getBoundingClientRect();
-      const width = containerWidth;
-      const height = containerHeight;
+    updateLayoutDimensions()
+    window.addEventListener('resize', updateLayoutDimensions)
+    return () => window.removeEventListener('resize', updateLayoutDimensions)
+  }, [])
 
-      setLayout({
-        width,
-        height,
-        paper_bgcolor: darkMode ? geekblue[9] : geekblue[0],
-        plot_bgcolor: darkMode ? geekblue[9] : geekblue[0],
-        title: {
-          text: chartTitle,
-          font: { size: 16, color: darkMode ? gray[0] : gray[5] }
-        },
-        xaxis: {
-          title: { text: 'Frequency (Hz)', font: { size: 16, color: darkMode ? gray[0] : gray[5] } },
-          type: 'log',
-          autorange: true,
-          showline: true,
-          linecolor: darkMode ? gray[0] : gray[5],
-          linewidth: 2,
-          tickfont: { color: darkMode ? gray[0] : gray[5] },
-          gridcolor: darkMode ? gray[0] : gray[5],
-          zerolinecolor: darkMode ? gray[0] : gray[5]
-        },
-        yaxis: {
-          title: { text: yAxisLabel, font: { size: 16, color: darkMode ? gray[0] : gray[5] } },
-          showline: true,
-          linecolor: darkMode ? gray[0] : gray[5],
-          linewidth: 2,
-          tickfont: { color: darkMode ? gray[0] : gray[5] },
-          gridcolor: darkMode ? gray[0] : gray[5],
-          zerolinecolor: darkMode ? gray[0] : gray[5]
-        }
-      });
-    }
-  }, [darkMode, chartTitle, yAxisLabel]);
+  useEffect(() => {
+    updateLayoutDimensions()
+
+    setLayout((prevLayout) => ({
+      ...prevLayout,
+      paper_bgcolor: darkMode ? geekblue[9] : geekblue[0],
+      plot_bgcolor: darkMode ? geekblue[9] : geekblue[0],
+      title: {
+        text: chartTitle,
+        font: { size: 16, color: darkMode ? gray[0] : gray[5] }
+      },
+      xaxis: {
+        title: { text: 'Frequency (Hz)', font: { size: 16, color: darkMode ? gray[0] : gray[5] } },
+        type: 'log',
+        autorange: true,
+        showline: true,
+        linecolor: darkMode ? gray[0] : gray[5],
+        linewidth: 2,
+        tickfont: { color: darkMode ? gray[0] : gray[5] },
+        gridcolor: darkMode ? gray[0] : gray[5],
+        zerolinecolor: darkMode ? gray[0] : gray[5]
+      },
+      yaxis: {
+        title: { text: yAxisLabel, font: { size: 16, color: darkMode ? gray[0] : gray[5] } },
+        showline: true,
+        linecolor: darkMode ? gray[0] : gray[5],
+        linewidth: 2,
+        tickfont: { color: darkMode ? gray[0] : gray[5] },
+        gridcolor: darkMode ? gray[0] : gray[5],
+        zerolinecolor: darkMode ? gray[0] : gray[5]
+      }
+    }))
+  }, [darkMode, chartTitle, yAxisLabel])
 
   return (
-    <div key={chartId} className="BodePlot" style={{ display: 'flex', flexDirection: 'column', width: '100%', height: '100%' }} ref={chartRef}>
-      <Plot data={measurements.map((m) => ({ x: frequencies, y: m, type: 'scatter', mode: 'lines+markers' }))} layout={layout} />
+    <div
+      key={chartId}
+      className="BodePlot"
+      style={{ display: 'flex', flexDirection: 'column', width: '100%', height: '100%' }}
+      ref={chartRef}
+    >
+      <Plot
+        data={measurements.map((m) => ({
+          x: frequencies,
+          y: m,
+          type: 'scatter',
+          mode: 'lines+markers'
+        }))}
+        layout={layout}
+      />
     </div>
   )
 }
